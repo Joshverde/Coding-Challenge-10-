70692cfaf534cce9fc258c3aaaa672ff5077aafe
Joshverde <52259302+Joshverde@users.noreply.github.com>
2025-02-23 01:41:01 -0500

    Task 3 - Created Inventory Class

--- cc_10.js
+++ cc_10.js
@@ -47,3 +47,29 @@ console.log("Task 2"); // for readability
 const order1 = new Order(501, prod1, 2); //creates a new order
 console.log(order1.getOrderDetails()); // calls and logs method
 console.log(prod1.getDetails()); // calls and logs method
+// Task 3
+
+class Inventory {
+    //creates a class
+    constructor() {
+      // creates a constructor
+      this.products = []; //creates an empty list
+      this.orders = []; //creates an empty list
+    }
+    addProduct(product) {
+      // creates method
+      this.products.push(product); // adds product to products list
+    }
+    listProducts() {
+      // creates method
+      this.products.forEach((product) => {
+        // uses foreach to console log each prodcut in products details
+        console.log(product.getDetails()); // logd product.getDetails to console
+      });
+    }
+}
+// Task 3 Test casess
+console.log("task 3"); // for ease of readability
+const inventory = new Inventory(); //creates a new inventory
+inventory.addProduct(prod1); // calls method
+inventory.listProducts(); // calls method
